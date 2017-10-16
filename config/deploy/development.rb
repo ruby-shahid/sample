d1eaddb0e5d77b6f6abf0aa625c087c8e4d10ceb
Syed Shahid <sshahid57@gmail.com>
2017-10-17 00:10:43 +0530

    change of server

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,10 +1,10 @@
-server 'ubuntu@ec2-13-126-226-33.ap-south-1.compute.amazonaws.com', user: 'deploy', roles: %w{web app db}
+server 'ubuntu@ec2-35-154-245-230.ap-south-1.compute.amazonaws.com', user: 'deploy', roles: %w{web app db}
 
-# set :ssh_options, {
-#   keys: %w(~/.ssh/id_rsa, ~/.ssh/),
-#   forward_agent: true,
-#   auth_methods: %w(publickey)
-# }
+set :ssh_options, {
+  keys: %w(~/.ssh/id_rsa),
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
 
 #set :puma_env, 'development'
 #set :nginx_server_name, 'ubuntu@ec2-13-126-182-184.ap-south-1.compute.amazonaws.com'
